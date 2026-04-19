a16b3f66f2b9afc5e6c4e4eeb9842694708382de
finogeev_vn <freesity.ru@gmail.com>
2026-04-19 18:53:56 +0300

    fix: change import

--- hexlet_code/__init__.py
+++ hexlet_code/__init__.py
@@ -1,4 +1,4 @@
-from .scripts.diff_builder import generate_diff
+from hexlet_code.scripts.diff_builder import generate_diff
 
 # Экспортируем generate_diff как основной интерфейс пакета
 __all__ = ['generate_diff']
